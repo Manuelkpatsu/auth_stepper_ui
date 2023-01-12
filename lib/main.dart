003f4fb6489dafd75022d4557875d860b8673eda
Emmanuel Ahuno <manuelkpatsu@gmail.com>
2023-01-12 09:15:33 +0000

    Change app description

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Auth UI Example',
       theme: CustomTheme.theme,
       home: AnimatedSplashScreen(
         duration: 3000,
