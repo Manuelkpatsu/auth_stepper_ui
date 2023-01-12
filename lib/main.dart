698e83c19c7708fee468a9d40fe1e8dc0ed110e6
Emmanuel Ahuno <manuelkpatsu@gmail.com>
2023-01-12 09:13:54 +0000

    Implement an animated splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:auth_ui/theme/custom_color.dart';
 import 'package:auth_ui/theme/custom_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
 
 import 'router.dart';
 import 'screen/auth/login/login_screen.dart';
@@ -17,7 +20,15 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: CustomTheme.theme,
-      home: const LoginScreen(),
+      home: AnimatedSplashScreen(
+        duration: 3000,
+        splash: Image.asset('assets/images/splash.png', fit: BoxFit.cover),
+        nextScreen: const LoginScreen(),
+        splashTransition: SplashTransition.scaleTransition,
+        pageTransitionType: PageTransitionType.fade,
+        backgroundColor: CustomColor.primaryColor,
+        splashIconSize: 150,
+      ),
       onGenerateRoute: AppRouter.generateRoute,
     );
   }
